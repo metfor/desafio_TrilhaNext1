ae642101e25001e502b6f6cb7bf7a7a1fa4608f0
metfor <mateus_frade@hotmail.com>
2021-10-04 16:08:52 -0300

    feat: marcar tarefa como completa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -27,7 +27,11 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completeTask = tasks.map(task=>task.id === id ? {
+      ...task,
+      isComplete:!task.isComplete
+    }:task)
+    setTasks(completeTask)
   }
 
   function handleRemoveTask(id: number) {
